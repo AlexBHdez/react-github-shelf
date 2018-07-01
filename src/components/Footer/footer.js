0a8cddac58ffc4688ab46cc857342a8fa6cb3aeb
AlexBHdez <abhbretones@gmail.com>
2018-07-01 11:02:35 +0200

    added some style to layout

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,14 +2,17 @@ import React from 'react';
 // CONFIG
 import { CURRENT_YEAR } from '../../config';
 // CSS
-import FaGithubAlt from 'react-icons/lib/fa/github-alt';
+import style from './footer.css';
+import FaCode from 'react-icons/lib/fa/code';
 
 const Footer = () => (
-  <div className="container-fluid">
-    Made with 
-    <FaGithubAlt/>
-    by <a href="http://www.alexbretones.com" target="_blank" rel="noopener noreferrer">AlexBHdez</a> ©{ CURRENT_YEAR }
-  </div>
+  <footer className={`container-fluid ${style.footer}`}>
+    <p>
+      Made with
+      <FaCode className={style.icon} />
+      by <a href="http://www.alexbretones.com" target="_blank" rel="noopener noreferrer">AlexBHdez</a> ©{ CURRENT_YEAR }
+    </p>
+  </footer>
 )
 
 export default Footer;
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 // CSS Modules
 import style from './header.css';
+import FaGithubAlt from 'react-icons/lib/fa/github-alt';
 
 const Header = () => (
   <header className={`container-fluid ${style.header}`}>
+    <div className={style.logoWrapper}>
+      <FaGithubAlt className={style.logoIcon} />
+    </div>
     <div className={style.headerContent}>
       React Github Shelf
     </div>
