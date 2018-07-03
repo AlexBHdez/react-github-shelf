6fdcf8d42ff5c11bbaf2ac41af96a255f0c90970
AlexBHdez <abhbretones@gmail.com>
2018-07-03 12:20:14 +0200

    better switch code in blankState widget component

--- src/Widgets/BlankState/blankState.js
+++ src/Widgets/BlankState/blankState.js
@@ -7,73 +7,71 @@ let blankStates = {
     title: 'Type to search',
     message: `If you start typing in the input below(more than 2 letters please), I will search
   for you in the Github Users Database.`,
+    image: {
+      url: '/img/blank-state-00.png',
+      alt: 'blank-state-00',
+    }
+      
   },
   noUser: {
     title: 'No user found',
     message: `Sorry, I used the force, but I didn 't found any users with that name. Please, type another username.`,
+    image: {
+      url: '/img/blank-state-01.png',
+      alt: 'blank-state-01',
+    }
   },
   error: {
     title: 'You got me',
-    message: `Sorry, some error ocurred with the github api. Please, reload the page and try again.`
+    message: `Sorry, some error ocurred with the github api. Please, reload the page and try again.`,
+    image: {
+      url: '/img/blank-state-02.png',
+      alt: 'blank-state-02',
+    }
   }
 }
 
 const BlankState = (props) => {
-  let template = null;
+  let title = null;
+  let content = null;
+  let image = null;
 
   switch (props.message) {
     case 0:
-      template = (
-        <div className={style.blankStateWrapper}>
-          <div className={style.message}>
-            <h2>
-              {blankStates.start.title}
-            </h2>
-            {blankStates.start.message}
-          </div>
-          <div className={style.imageWrapper}>
-            <img className={style.image} src="/img/blank-state-00.png" alt="blank-state"/>
-          </div>
-        </div>
-      )
+      title = blankStates.start.title;
+      content = blankStates.start.message;
+      image = <img className={style.image} src={blankStates.start.image.url} alt={blankStates.start.image.alt}/>;
     break;
     
     case 1:
-      template = (
-        <div className={style.blankStateWrapper}>
-          <div className={style.message}>
-            <h2>
-              {blankStates.noUser.title}
-            </h2>
-            {blankStates.noUser.message}
-          </div>
-          <div className={style.imageWrapper}>
-            <img className={style.image} src="/img/blank-state-01.png" alt="blank-state"/>
-          </div>
-        </div>
-      )
+      title = blankStates.noUser.title;
+      content = blankStates.noUser.message;
+      image = <img className={style.image} src={blankStates.noUser.image.url} alt={blankStates.start.image.alt}/>;
     break;
 
     case 2:
-      template = (
-        <div className={style.blankStateWrapper}>
-          <div className={style.message}>
-            <h2>
-              {blankStates.error.title}
-            </h2>
-            {blankStates.error.message}
-          </div>
-          <div className={style.imageWrapper}>
-            <img className={style.image} src="/img/blank-state-02.png" alt="blank-state"/>
-          </div>
-        </div>
-      )
+      title = blankStates.error.title;
+      content = blankStates.error.message;
+      image = <img className={style.image} src={blankStates.error.image.url} alt={blankStates.start.image.alt}/>;
     break;
   
     default:
-      template = null;
+      content = null;
+      image = null;
   }
-  return template;
+  return (
+    <div className={style.blankStateWrapper}>
+      <div className={style.message}>
+        <h2>
+          {title}
+        </h2>
+          {content}
+      </div>
+      <div className={style.imageWrapper}>
+        {image}
+      </div>
+    </div>
+  )
 }
 
 export default BlankState;
