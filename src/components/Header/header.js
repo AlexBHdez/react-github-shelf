f5b642f14b86af02321eb4e2e9551d5b1951a8e3
AlexBHdez <abhbretones@gmail.com>
2018-07-14 18:36:13 +0200

    cleaning comments

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,10 +3,6 @@ import { Link, withRouter } from 'react-router-dom';
 // CSS Modules
 import style from './header.css';
 
-// const RouteNames = {
-//   home: '/react-github-shelf/'
-// }
-
 const Header = () => (
   <header className="container-fluid">
     <Link to="/">
